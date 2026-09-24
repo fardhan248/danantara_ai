362f54876a273e71284043a80dcdea4ee3f4fa37
fardhan248 <findrayesa@gmail.com>
2026-09-24 09:40:59 +0700

    chore: remove unused reranker

--- langgraph_app/main_app.py
+++ langgraph_app/main_app.py
@@ -5,7 +5,6 @@ from utils.db_pool import close_db_pool, get_db_pool
 from contextlib import asynccontextmanager
 from miniopy_async import Minio
 from setup_db import start_setup
-from models.openai import LLamaCppReranker
 import utils.contextmanager_utils as cm
 import chromadb, os
 
@@ -24,13 +23,10 @@ async def lifespan(app: FastAPI):
         secret_key=MINIO_ROOT_PASSWORD,
         secure=False,
     )
-    cm.reranker = LLamaCppReranker(api_key=LLAMA_CPP_KEY)
 
-    await cm.reranker.start()
     app.state.pool = await get_db_pool()
     yield
     await close_db_pool()
-    await cm.reranker.close()
     
 app = FastAPI(lifespan=lifespan)
 
--- langgraph_app/utils/contextmanager_utils.py
+++ langgraph_app/utils/contextmanager_utils.py
@@ -1,7 +1,5 @@
 from chromadb.api import ClientAPI
 from miniopy_async import Minio
-from models.openai import LLamaCppReranker
 
 minio: Minio | None = None
 chroma: ClientAPI | None = None
-reranker: LLamaCppReranker | None = None
